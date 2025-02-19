847d31d63dff3392c6cbce81a3ed1e3fc6e7ef15
diak <diak@mesterlevel.lo>
2025-02-19 10:03:05 +0100

    ASDSADADASD

--- js/profile.js
+++ js/profile.js
@@ -1,10 +1,11 @@
-window.addEventListener('DOMContentLoaded', getusername);
+window.addEventListener('DOMContentLoaded',getusername );
 
-const saveBtn = document.getElementById('saveBtn');
+const saveBtn=document.getElementById('saveBtn');
 saveBtn.addEventListener('click', editData);
-const homeBtn = document.getElementsByClassName('icon-home')[0];
-const userBtn = document.getElementsByClassName('icon-user')[0];
-const cartBtn = document.getElementsByClassName('icon-cart')[0];
+const homeBtn=document.getElementsByClassName('icon-home')[0];
+const userBtn=document.getElementsByClassName('icon-user')[0];
+const cartBtn=document.getElementsByClassName('icon-cart')[0];
+   
 
 const btnpHistory = document.getElementsByClassName('btnpHistory')[0];
 const btnEditPfp = document.getElementsByClassName('btnEditPfp')[0];
@@ -13,84 +14,99 @@ const btnSupport = document.getElementsByClassName('btnSupport')[0];
 const btnBack = document.getElementsByClassName('btnBack')[0];
 
 async function getusername() {
-    const res = await fetch('https://nodejs312.dszcbaross.edu.hu/api/profile/Myusername', {
+    const res = await fetch('http://127.0.0.1:3000/api/profile/Myusername', {
         method: 'GET',
         credentials: 'include'
     });
     const username = await res.json();
     console.log(username);
     profileData(username);
-
+    
     if (res.ok) {
-        const editPic = document.querySelector('.profile_pic');
-        editPic.src = `https://nodejs312.dszcbaross.edu.hu/uploads/${username[0].profile_pic}`;
+        const editPic = document.querySelectore('.profile_pic');
+        editPic.style.backgroundImage = `url('http://127.0.0.1:3000/uploads/${username[0].profile_pic}')`;
     }
 }
 
-function profileData(users) {
-    const alapkep = './img/logo.png'; // Relatív elérési út
+function profileData(users)
+{
+    //const alapkep = './img/logo.png'; // Relatív elérési út
     const username = document.getElementById('username');
-    const fullname = document.getElementById('fullname');
+    const fullname = document.getElementById('fullname');    
     const postcode = document.getElementById('postal-code');
     const city = document.getElementById('city');
     const street = document.getElementById('street');
 
-    // Alapértékek tisztázása
-    username.innerHTML = '';
-    fullname.value = '';
-    postcode.value = '';
-    city.value = '';
-    street.value = '';
+// Alapértékek tisztázása
+username.innerHTML = '';
+fullname.value = '';
+postcode.value = '';
+city.value = '';
+street.value = '';
 
-    for (const user of users) {
-        username.textContent = user.fullname;
-        fullname.value = user.fullname;
-        postcode.value = user.postcode;
-        city.value = user.city;
-        street.value = user.street;
-    }
+for (const user of users) {
+    username.textContent = user.fullname;
+    fullname.value = user.fullname;
+    postcode.value = user.postcode;
+    city.value = user.city;
+    street.value = user.street;  
 }
+
+
+
+}
+
+
+
 
 btnLogout.addEventListener('click', logout);
 
-homeBtn.addEventListener('click', () => {
-    window.location.href = 'https://erikk7274.github.io/TechBay//home.html';
-});
+homeBtn.addEventListener('click',()=>{
+    window.location.href='../home.html';
+})
 
-userBtn.addEventListener('click', () => {
-    window.location.href = 'https://erikk7274.github.io/TechBay/profile.html';
-});
+userBtn.addEventListener('click',()=>{
+    window.location.href='../profile.html';
+})
 
-cartBtn.addEventListener('click', () => {
-    window.location.href = 'https://erikk7274.github.io/TechBay/cart.html';
-});
+cartBtn.addEventListener('click',()=>{
+    window.location.href='../cart.html';
+})
 
 if (btnpHistory) {
     btnpHistory.addEventListener('click', () => {
-        window.location.href = 'https://erikk7274.github.io/TechBay/purchaseHistory.html';
+        window.location.href = '../purchaseHistory.html';
     });
 }
 
 if (btnBack) {
     btnBack.addEventListener('click', () => {
-        window.location.href = 'https://erikk7274.github.io/TechBay/home.html';
+        window.location.href = '../home.html';
     });
 }
 
 if (btnEditPfp) {
     btnEditPfp.addEventListener('click', () => {
-        window.location.href = 'https://erikk7274.github.io/TechBay/profilePic.html';
+        window.location.href = '../profilePic.html';
     });
 }
+
+
 
 if (btnSupport) {
     btnSupport.addEventListener('click', () => {
-        window.location.href = 'https://erikk7274.github.io/TechBay/support.html';
+        window.location.href = '../support.html';
     });
 }
 
+
+// function toggleSearch() {
+//     const searchMenu = document.getElementById('keresomenu');
+//     searchMenu.classList.toggle('active');
+// }
+
 async function logout() {
-    const res = await fetch('https://nodejs312.dszcbaross.edu.hu/api/auth/logout', {
+    const res = await fetch('http://127.0.0.1:3000/api/auth/logout', {
         method: 'POST',
         credentials: 'include'
     });
@@ -99,27 +115,29 @@ async function logout() {
 
     if (res.ok) {
         alert(data.message);
-        window.location.href = 'https://erikk7274.github.io/TechBay/index.html';
+        window.location.href = '../index.html';
     } else {
         alert('Hiba a kijelentkezéskor');
     }
 }
 
-async function editData() {
-    const postcode = document.getElementById('postal-code').value;
-    const city = document.getElementById('city').value;
-    const street = document.getElementById('street').value;
-    const fullname = document.getElementById('fullname').value;
-    if (!postcode || !city || !street || !fullname) {
-        alert("Minden mezőt ki kell tölteni");
+async function editData(){
+    const postcode=document.getElementById('postal-code').value;
+    const city=document.getElementById('city').value;
+    const street=document.getElementById('street').value;
+    const fullname=document.getElementById('fullname').value;
+    console.log(postcode,fullname,city,street);
+    if(!postcode ||!city||!street||!fullname){
+        alert("Minden mezőt ki kell tölteni")
     }
-    const res = await fetch('https://nodejs312.dszcbaross.edu.hu/api/profile/editData', {
-        method: 'PUT',
-        headers: {
-            'content-type': 'application/json'
+    else{
+        const res=await fetch('http://127.0.0.1:3000/api/profile/editData',{
+        method:'PUT',
+        headers:{
+            'content-type':'application/json'
         },
-        body: JSON.stringify({ postcode, city, street, fullname }),
-        credentials: 'include'
+        body:JSON.stringify({postcode,city,street,fullname}),
+        credentials:'include'
     });
     const data = await res.json();
     console.log(data);
@@ -127,7 +145,7 @@ async function editData() {
     if (res.ok) {
         resetInputs();
         alert(data.message);
-        window.location.href = 'https://erikk7274.github.io/TechBay/profile.html';
+        window.location.href = '../profile.html';
     } else if (data.errors) {
         let errorMessage = '';
         for (let i = 0; i < data.errors.length; i++) {
@@ -139,6 +157,9 @@ async function editData() {
     } else {
         alert('Ismeretlen hiba');
     }
+    }
+    
+    
 }
 
 window.addEventListener('click', function (event) {
@@ -146,12 +167,13 @@ window.addEventListener('click', function (event) {
     const menu = document.querySelector('nav');
     const hamburgerButton = document.querySelector('.hamburger-menu');
 
+
     if (!hamburgerButton.contains(event.target) && !menu.contains(event.target) && !menuToggle.contains(event.target)) {
-        menuToggle.checked = false;
+        menuToggle.checked = false; 
     }
 });
 
-function resetInputs() {
+function resetInputs(){
     document.getElementById('postal-code').value = '';
     document.getElementById('city').value = '';
     document.getElementById('street').value = '';
