1c8db6ef2dfd70ab8e987be7aabf3842125fc1d9
diak <diak@mesterlevel.lo>
2025-02-21 11:59:19 +0100

    111111

--- js/preBuilt.js
+++ js/preBuilt.js
@@ -1,18 +1,31 @@
 // Gombok deklarálása
-const buttons = {
-    back: document.querySelector('.btnBack'),
+const gombok = {
+    vissza: document.querySelector('.btnBack'),
+    modaltMutat: document.querySelector('.fixed') // Az elem, ami megnyitja a modalt
 };
+
+// Modal inicializálása
+const modalElem = document.getElementById('myModal');
+const modal = new bootstrap.Modal(modalElem, {
+    keyboard: false // A modal csak a kiváltó esemény hatására nyílik meg
+});
 
 // DOM betöltődés eseménykezelője
 window.addEventListener('DOMContentLoaded', () => {
-    setupButtonHandlers();
+    gombokBeallitas();
 });
 
 // Gomb eseménykezelők beállítása
-function setupButtonHandlers() {
-    if (buttons.back) {
-        buttons.back.addEventListener('click', () => {
+function gombokBeallitas() {
+    if (gombok.vissza) {
+        gombok.vissza.addEventListener('click', () => {
             window.location.href = 'https://erikk7274.github.io/TechBay/home.html';
+        });
+    }
+
+    if (gombok.modaltMutat) {
+        gombok.modaltMutat.addEventListener('click', () => {
+            modal.show(); // A modal megjelenítése, amikor a .fixed elemre kattintunk
         });
     }
 }
