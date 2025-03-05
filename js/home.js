c617cb9848572ab4d4354eb8bb9b14b30f237e39
diak <diak@mesterlevel.lo>
2025-03-05 13:17:36 +0100

    css and html

--- js/home.js
+++ js/home.js
@@ -2,9 +2,9 @@ window.addEventListener('DOMContentLoaded', getProducts);
 
 // Gombok lekérése
 const btnPreBuilt = document.getElementsByClassName('btnPrebuilt')[0];
-const homeBtn = document.getElementsByClassName('icon-home')[0];
-const userBtn = document.getElementsByClassName('icon-user')[0];
-const cartBtn = document.getElementsByClassName('icon-cart')[0];
+const homeBtn=document.getElementsByClassName('icon-home')[0];
+const userBtn=document.getElementsByClassName('icon-user')[0];
+const cartBtn=document.getElementsByClassName('icon-cart')[0];
 const btnLogout = document.getElementsByClassName('icon-logout')[0];
 
 // Kategóriák elemeinek lekérése
@@ -13,7 +13,7 @@ const row = document.getElementById('row');
 // Termékek lekérése
 async function getProducts() {
     try {
-        const response = await fetch(`/api/getProducts/getProducts_all`, {
+        const response = await fetch(/api/getProducts/getProducts_all, {
             method: 'GET',
             credentials: 'include'
         });
@@ -21,54 +21,49 @@ async function getProducts() {
         const products = await response.json();
         console.log(products);
 
-        console.log(`Lekért termékek:`, products);
+        console.log(Lekért termékek:, products);
 
+        //renderCategory(category, result);
         renderProducts(products);
     } catch (error) {
         console.error('Hiba a termékek lekérésekor:', error);
     }
 }
 
+
 function renderProducts(products) {
     console.log(products);
-    let html = `<h2>Our Products</h2><div class="row">`;
-
+    let html = <h2>asdsad</h2><div class="products">;
     products.forEach(product => {
         console.log(product);
-        html += `
-        <div class="col-md-4">
-            <div class="card mb-4 shadow-sm product-card">
-                <img src='/uploads/${product.product_pic}' alt="${product.product_name}" class="card-img-top" />
-                <div class="card-body">
-                    <h5 class="card-title">${product.product_name}</h5>
-                    <p class="card-text">${product.price} Ft</p>
-                    <button class="btn btn-primary btn-add-to-cart">Add to Cart</button>
-                </div>
-            </div>
+        html += 
+        <div class="product">
+            <img src='/uploads/${product.product_pic}' alt="${product.product_name}" />
+            <h3>${product.product_name}</h3>
+            <p>${product.price}</p>
         </div>
-    `;
+    ;
     });
 
     html += '</div>';
 
+    console.log(html);
 
     row.innerHTML = html;
 }
-
 btnLogout.addEventListener('click', logout);
 
-homeBtn.addEventListener('click', () => {
-    window.location.href = 'https://techbay2.netlify.app/home.html';
-});
+homeBtn.addEventListener('click',()=>{
+    window.location.href='https://techbay2.netlify.app/home.html';
+})
 
-userBtn.addEventListener('click', () => {
-    window.location.href = 'https://techbay2.netlify.app/profile.html';
-});
+userBtn.addEventListener('click',()=>{
+    window.location.href='https://techbay2.netlify.app/profile.html';
+})
 
-cartBtn.addEventListener('click', () => {
-    window.location.href = 'https://techbay2.netlify.app/cart.html';
-});
-
-btnPreBuilt.addEventListener('click', () => {
-    window.location.href = 'https://techbay2.netlify.app/preBuilt.html';
-});
+cartBtn.addEventListener('click',()=>{
+    window.location.href='https://techbay2.netlify.app/cart.html';
+})
+btnPreBuilt.addEventListener('click',()=>{
+    window.location.href='https://techbay2.netlify.app/preBuilt.html';
+})
