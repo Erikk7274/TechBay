3518fce51000bc27e82afc03c5a1403534704c74
diak <diak@mesterlevel.lo>
2025-03-20 12:18:41 +0100

    pcbuilding stuff

---
+++ js/pcBuilding.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', () => {
+    getProducts();
+    setupEventListeners();
+});
+
+function setupEventListeners() {
+    const btnLogout = document.querySelector('.icon-logout');
+    const btnBack = document.querySelector('.btnBack');
+
+    // Vissza gomb eseménykezelő
+    btnBack?.addEventListener('click', () => window.location.href = 'https://techbay2.netlify.app/home.html');
+
+    // Kijelentkezés gomb eseménykezelő
+    if (btnLogout) {
+        btnLogout.addEventListener('click', logout);
+    } else {
+        console.error('Nem található kijelentkezés gomb.');
+    }
+}
+
+// Kijelentkezés függvény
+async function logout() {
+    try {
+        const res = await fetch('/api/auth/logout', {
+            method: 'POST',
+            credentials: 'include'
+        });
+
+        if (res.ok) {
+            // Tokenek törlése
+            localStorage.removeItem('token');
+            sessionStorage.removeItem('token');
+
+            alert('Sikeres kijelentkezés');
+            window.location.href = 'https://techbay2.netlify.app/index.html';
+        } else {
+            alert('Hiba a kijelentkezéskor');
+        }
+    } catch (error) {
+        console.error('Hiba a kijelentkezés során:', error);
+    }
+}
+
