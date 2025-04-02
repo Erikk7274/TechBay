28242c1f220ed6bc65511534f4ea08ec68783da8
diak <diak@mesterlevel.lo>
2025-04-02 09:45:00 +0200

    fixing home js

--- js/preBuilt.js
+++ js/preBuilt.js
@@ -60,11 +60,10 @@ function renderProducts(products) {
 }
 
 function createCard(product) {
-
-
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card', 'm-3', 'p-2', 'shadow-sm');
     cardDiv.style.width = '18rem';
+
     let priceHtml = product.sale && product.sale < product.price
         ? `<span class="d-block mb-2" style="text-decoration: line-through;">Ár: ${product.price} Ft</span>`
         : `<span class="d-block mb-2">Ár: ${product.price ? product.price + ' Ft' : 'N/A'}</span>`;
