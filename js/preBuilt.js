317a9cb15d4f56446ced0323be510be80004b702
diak <diak@mesterlevel.lo>
2025-04-02 12:41:59 +0200

    fixing home js

--- js/preBuilt.js
+++ js/preBuilt.js
@@ -107,7 +107,7 @@ function createModal(product) {
                     <img src="/api/uploads/${product.config_pic}" alt="${product.config_name || product.config_name}" class="img-fluid mb-3">
                     <p><strong>Raktáron:</strong> ${product.in_stock}</p>
                     <p><strong>Ár:</strong> ${product.price ? `${product.price} Ft` : 'N/A'}</p>
-                    <p><strong>Leírás:</strong><br> ${product.product_description}</p>
+                    <p><strong>Leírás:</strong><br> ${product.description}</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary add-to-cart-btn" data-product-id="${product.config_id}" data-bs-dismiss="modal">Kosárba</button>
