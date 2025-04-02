e698002a141352dbc56a5f3b0006764975835487
diak <diak@mesterlevel.lo>
2025-04-02 09:20:18 +0200

    fixing home js

--- js/home.js
+++ js/home.js
@@ -62,9 +62,11 @@ function renderProducts(products) {
 
 function createCard(product) {
     const cardDiv = document.createElement('div');
-    cardDiv.classList.add('card', 'm-2', 'p-2', 'shadow-sm');
-    cardDiv.style.width = '18rem';
-    cardDiv.style.minHeight = '20rem';
+    cardDiv.classList.add('col-md-4', 'd-flex'); 
+    const cardInnerDiv = document.createElement('div');
+    cardInnerDiv.classList.add('card', 'm-2', 'p-2', 'shadow-sm', 'w-100', 'd-flex', 'flex-column'); 
+
+    cardInnerDiv.style.minHeight = '22rem';
 
     let priceHtml = product.sale && product.sale < product.price 
         ? `<span class="d-block mb-2" style="text-decoration: line-through;">Ár: ${product.price} Ft</span>`
